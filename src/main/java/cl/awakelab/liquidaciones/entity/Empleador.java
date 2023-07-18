7377acfd36b78b54a2e26f8a055a16311c3af021
joaquin <joaquinkappa@hotmail.com>
2023-07-18 12:03:15 -0400

    crud empleador listo

--- src/main/java/cl/awakelab/liquidaciones/entity/Empleador.java
+++ src/main/java/cl/awakelab/liquidaciones/entity/Empleador.java
@@ -33,8 +33,8 @@ public class Empleador {
     private String email;
 
     //relacion con tabla usuario
-    @ManyToOne(optional = false, cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-    @JoinColumn(name = "id_usuario", nullable = false)
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "id_usuario")
     private Usuario usuario;
 
     @Column
